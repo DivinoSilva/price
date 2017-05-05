d09d7ab65998dff055ca6d14f74a4cca581fecc2
DivinoSilva <fernandinho_07@hotmail.com>
2017-05-05 18:43:10 -0300

    Substituído DB MySQL para DB HSQL

--- src/main/java/br/com/price/conf/JPAConfiguration.java
+++ src/main/java/br/com/price/conf/JPAConfiguration.java
@@ -22,27 +22,28 @@ public class JPAConfiguration {
 				new LocalContainerEntityManagerFactoryBean();
 		
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-		
 		factoryBean.setJpaVendorAdapter(vendorAdapter);
 		
+		//Trecho de configuração para usar DB MySQL
+		/*
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setUsername("root");
 		dataSource.setPassword("divino");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/price");
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");*/
 		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("SA");
+		dataSource.setPassword("");
+		dataSource.setUrl("jdbc:hsqldb:file:C:/banco/price;hsqldb.lock_file=false");
+		dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
 		factoryBean.setDataSource(dataSource);
-		
 		Properties props = new Properties();
-		
-		
-		props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		props.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
 		props.setProperty("hibernate.show_sql", "true");
 		props.setProperty("hibernate.hbm2ddl.auto", "update");
-		
 		factoryBean.setJpaProperties(props);
 		factoryBean.setPackagesToScan("br.com.price.model");
-		
 		return factoryBean;
 	}
 	
